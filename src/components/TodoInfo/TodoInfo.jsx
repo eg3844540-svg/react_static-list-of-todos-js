a89f1bd737a37254a8732a6908827c3f87b3622e
ericagomes-dev <ericagomesdev@gmail.com>
2026-06-07 19:04:50 -0300

    Fix TodoInfo component

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,15 @@
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo }) => (
+  <ul>
+    <li>
+      <strong>Title:</strong> {todo.title}
+    </li>
+
+    <li>
+      <strong>User:</strong> {todo.user && todo.user.name}
+    </li>
+    <li>
+      <strong>Completed:</strong> {todo.completed ? 'Yes' : 'No'}
+    </li>
+  </ul>
+);
